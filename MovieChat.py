f48001a80de3183d9c63a0122872faf33661c330
staythepath <landonmitchell@gmail.com>
2023-11-07 18:04:30 -0600

    This is better than ever.

--- MovieChat.py
+++ MovieChat.py
@@ -5,10 +5,9 @@ import openai
 from tmdbv3api import TMDb, Movie
 from dotenv import load_dotenv
 import re
-import requests
 import asyncio
 from arrapi import RadarrAPI
-import more_itertools
+import time
 
 # Load environment variables from .env file
 load_dotenv()
@@ -18,8 +17,7 @@ TMDB_API_KEY = os.getenv('TMDB_API_KEY')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 # Radarr settings
-RADARR_URL = os.getenv("RADARR_URL")  # Replace with your Radarr URL
-# Replace with your Radarr API key
+RADARR_URL = os.getenv("RADARR_URL")
 RADARR_API_KEY = os.getenv("RADARR_API_KEY")
 
 # Configure TMDb with the API key
@@ -35,72 +33,56 @@ radarr = RadarrAPI(RADARR_URL, RADARR_API_KEY)
 # Configure OpenAI API key
 openai.api_key = OPENAI_API_KEY
 
-# Function to get response from OpenAI API
 def get_openai_response(prompt):
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4-1106-preview",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant with a ton of movie knowledge. Whenever you count things or list them go 1 through 9 then go to A then B then C and so on until K.  Please place any movie titles in single quotes."},
+            {"role": "system", "content": "First off, never list more than 20 movies in your response. It's very important that you never name or list more than 20 movies in your response. You are a helpful assistant with a ton of movie knowledge. Whenever you count things or list them go 1 through 9 then go to A then B then C and so on until K. Whenever you send a new message with a new list, even if it's a continuation of another list, always start over at 1 then count up to 9 then start at A and go to K. That is always how you will count lists. Whenever you use a movie title, use the title the exact way it is used on the TMDB website including capitalization, spelling, punctuation and spacing. It is imperative that the movie titles you give me match the titles on TMBDB perfectly. Whenever you use a movie title, please signify it by surrounding it with asterisks like *Movie Title* (Year). Never list or name more than 20 movies in a response even if I ask you to."},
             {"role": "user", "content": prompt},
         ],
         temperature=0,
     )
     return response.choices[0].message['content'].strip()
 
-# Use the OpenAI API to get a text response
-prompt = "Hey there. Do you like movies?"
-sample_text = get_openai_response(prompt)
-print(f"{sample_text}")
-
 def check_for_movie_title_in_string(text):
-    found_movies_dict = {}  # Dictionary to store found movie titles and their tmdbIds
+    movie_titles_map = {}
+    phrases_in_stars = re.findall(r"\*([^*]+)\* \(\d{4}\)", text)  # Adjusted regex
 
-    # Use regular expressions to find all phrases enclosed in single quotes
-    phrases_in_quotes = re.findall(r"'(.*?)'", text)
+    for phrase in phrases_in_stars:
+        search_phrase = phrase  # No need to escape apostrophes
+        results = movie.search(search_phrase)
+        print(f"\nSearch phrase: '{search_phrase}'")
+        print("Results:")
 
-    for phrase in phrases_in_quotes:
-        # Skip the check if the phrase is too long
-        if len(phrase.split()) > 10:
-            continue
+        time.sleep(0.1)  # Introduce a delay
 
-        print(f"Searching TMDb for: {phrase}")
-        results = movie.search(phrase)
-        for result in results:
-            movie_title = result.title
-            tmdbId = result.id  # Added line to capture tmdbId
-            popularity = result.popularity
-            vote_average = result.vote_average
-            vote_count = result.vote_count
+        for idx, result in enumerate(results):
+            if isinstance(result, dict) and 'title' in result and 'id' in result:
+                print(f"  Result {idx + 1}: {result}")
+                tmdb_title = result['title']
+            elif hasattr(result, 'title') and hasattr(result, 'id'):
+                print(f"  Result {idx + 1}: Title - {result.title}, ID - {result.id}")
+                tmdb_title = result.title
+            else:
+                print(f"  Result {idx + 1}: Invalid format")
+                continue
 
-            print(
-                f"Received from TMDb: {movie_title} with popularity {popularity}")
+            if tmdb_title:
+                movie_titles_map[phrase] = tmdb_title
+                break  # Stop after finding the first valid result
 
-            if (
-                movie_title.lower() in text.lower()
-                and vote_average >= 0
-                and vote_count >= 0
-                and popularity > 0
-            ):
-                # Only add the movie if it's not already in the dictionary
-                if movie_title not in found_movies_dict:
-                    found_movies_dict[movie_title] = {'title': movie_title, 'tmdbId': tmdbId}
-                    print(f"Added: {movie_title}")
+    return movie_titles_map
 
-    # Convert the dictionary back to a list of movies before returning it
-    found_movies = list(found_movies_dict.values())
-    print(found_movies)
-    return found_movies
 
-# Create a new Discord client
-# Create a new Discord client with intents
+
+
+
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
 client = commands.Bot(command_prefix='!', intents=intents)
 
 conversations = {}
-
-# Get the Discord token from environment variables
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
 @client.event
@@ -109,17 +91,21 @@ async def on_ready():
 
 @client.command(name='!')
 async def ask(ctx, *, question):
-    # Get the conversation history for this channel
+    emojis = [
+        '\u0031\uFE0F\u20E3', '\u0032\uFE0F\u20E3', '\u0033\uFE0F\u20E3', '\u0034\uFE0F\u20E3',
+        '\u0035\uFE0F\u20E3', '\u0036\uFE0F\u20E3', '\u0037\uFE0F\u20E3', '\u0038\uFE0F\u20E3', '\u0039\uFE0F\u20E3',
+        '\U0001F1E6', '\U0001F1E7', '\U0001F1E8', '\U0001F1E9', '\U0001F1EA', '\U0001F1EB', '\U0001F1EC', '\U0001F1ED',
+        '\U0001F1EE', '\U0001F1EF', '\U0001F1F0'
+    ]
+
     channel_id = str(ctx.channel.id)
     if channel_id not in conversations:
         conversations[channel_id] = [
-            {"role": "system", "content": "You are a helpful assistant with a ton of movie knowledge. Whenever you count things or list them go 1 through 9 then go to A then B then C and so on until K.  Please place any movie titles in single quotes."}
+            {"role": "system", "content": "First off, never list more than 20 movies in your response. It's very important that you never name or list more than 20 movies in your response. You are a helpful assistant with a ton of movie knowledge. Whenever you count things or list them go 1 through 9 then go to A then B then C and so on until K. Whenever you send a new message with a new list, even if it's a continuation of another list, always start over at 1 then count up to 9 then start at A and go to K. That is always how you will count lists. Whenever you use a movie title, use the title the exact way it is used on the TMDB website including capitalization, spelling, punctuation and spacing. It is imperative that the movie titles you give me match the titles on TMBDB perfectly. Whenever you use a movie title, please signify it by surrounding it with asterisks like *Movie Title* (Year). Never list or name more than 20 movies in a response even if I ask you to."}
         ]
 
-    # Add the user's message to the conversation history
     conversations[channel_id].append({"role": "user", "content": question})
 
-    # Get the response from OpenAI
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=conversations[channel_id],
@@ -127,57 +113,54 @@ async def ask(ctx, *, question):
         max_tokens=750
     ).choices[0].message['content'].strip()
 
-    # Add the OpenAI's response to the conversation history
-    conversations[channel_id].append(
-        {"role": "assistant", "content": response})
+    print(f"GPT Response: {response}")
 
-    found_movie_titles = check_for_movie_title_in_string(response)
+    conversations[channel_id].append({"role": "assistant", "content": response})
 
-    if found_movie_titles:
-        emojis = [
-    '\u0031\uFE0F\u20E3', '\u0032\uFE0F\u20E3', '\u0033\uFE0F\u20E3', '\u0034\uFE0F\u20E3',
-    '\u0035\uFE0F\u20E3', '\u0036\uFE0F\u20E3', '\u0037\uFE0F\u20E3', '\u0038\uFE0F\u20E3', '\u0039\uFE0F\u20E3',
-    '\U0001F1E6', '\U0001F1E7', '\U0001F1E8', '\U0001F1E9', '\U0001F1EA', '\U0001F1EB', '\U0001F1EC', '\U0001F1ED',
-    '\U0001F1EE', '\U0001F1EF', '\U0001F1F0'
-]
+    movie_titles_map = check_for_movie_title_in_string(response)
 
-        emoji_movie_map = dict(zip(emojis, found_movie_titles))  # Create map for this chunk
-        response_chunk = response
-        for emoji, movie in emoji_movie_map.items():
-            # Using regex to replace movie titles with title + emoji
-            pattern = re.compile(
-                re.escape(f"'{movie['title']}'"), re.IGNORECASE)
-            response_chunk = pattern.sub(f"'{movie['title']}' {emoji}", response_chunk)
+    number_of_movies_found = len(movie_titles_map)
+    print(f"Number of movies found in GPT response: {number_of_movies_found}")
+    
 
-        # Split the response_chunk into smaller chunks if it's too long
-        max_length = 2000
-        response_segments = [response_chunk[i:i+max_length] for i in range(0, len(response_chunk), max_length)]
+    response_chunk = response.replace("*", "'")
+    for index, (original_title, tmdb_title) in enumerate(movie_titles_map.items()):
+        emoji = emojis[index % len(emojis)]  # Loop over emojis if more movies than emojis
+        escaped_original_title = re.escape(original_title)
+        pattern = re.compile(f"'{escaped_original_title}'", re.IGNORECASE)
+        response_chunk = pattern.sub(f"'{tmdb_title}' {emoji}", response_chunk)
 
-        for response_segment in response_segments:
-            # Print the OpenAI response with emojis to the Discord chat
-            msg = await ctx.send(f"OpenAI: {response_segment}")
+    max_length = 2000
+    response_segments = [response_chunk[i:i+max_length] for i in range(0, len(response_chunk), max_length)]
 
-            for emoji in emoji_movie_map.keys():  # Updated line
-                await msg.add_reaction(emoji)
+    for response_segment in response_segments:
+        msg = await ctx.send(f"OpenAI:\n {response_segment}")
+        for emoji in emojis[:number_of_movies_found]:
+            await msg.add_reaction(emoji)
 
-            def check(reaction, user):
-                return user == ctx.message.author and str(reaction.emoji) in emoji_movie_map and reaction.message.id == msg.id
+        def check(reaction, user):
+            return user == ctx.message.author and str(reaction.emoji) in emojis[:number_of_movies_found] and reaction.message.id == msg.id
 
-            while True:
-                reaction, user = await client.wait_for('reaction_add', check=check)
-                movie = emoji_movie_map[str(reaction.emoji)]
+        last_reaction_time = time.time()
+        while True:
+            try:
+                reaction, user = await client.wait_for('reaction_add', timeout=30.0, check=check)  # Shorter timeout for each reaction
+                selected_emoji = str(reaction.emoji)
+                selected_movie = next((tmdb_title for original_title, tmdb_title in movie_titles_map.items() if f"'{tmdb_title}' {selected_emoji}" in response_segment), None)
 
-                # Here, using arrapi to add movie to Radarr
-                try:
-                    search = radarr.search_movies(movie['title'])
-                    if search:
-                        # Modify path and quality as needed
-                        search[0].add("/data/media/movies", "this")
-                        await ctx.send(f"Added '{movie['title']}' to Radarr.")
-                    else:
-                        await ctx.send(f"Failed to find '{movie['title']}' in Radarr database.")
-                except Exception as e:
-                    await ctx.send(f"You already have '{movie['title']}'!!")
+                if selected_movie:
+                    try:
+                        search = radarr.search_movies(selected_movie)
+                        if search:
+                            search[0].add("/data/media/movies", "this")
+                            await ctx.send(f"Added '{selected_movie}' to Radarr.")
+                        else:
+                            await ctx.send(f"Failed to find '{selected_movie}' in Radarr database.")
+                    except Exception as e:
+                        await ctx.send(f"You already have '{selected_movie}'!")
+                last_reaction_time = time.time()
+            except asyncio.TimeoutError:
+                if time.time() - last_reaction_time > 60:  # If no reactions for 60 seconds
+                    break  # Exit the loop if no reactions for a while
 
-# Run the Discord client
 client.run(DISCORD_TOKEN)
