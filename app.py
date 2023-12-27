9842d6e9e36e59ac9c69597ddca073c2175f1268
staythepath <landonmitchell@gmail.com>
2023-12-27 16:00:48 -0600

    working on it

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from config_manager import ConfigManager
 from flask import Flask, request, render_template, jsonify, session
 import yaml
 import os
@@ -15,107 +16,95 @@ import multiprocessing
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)  # Generate a random secret key
-global radarr 
+config_manager = ConfigManager
+global radarr
 channels_data = []
 
-def start_discord_bot_process():
-    subprocess.Popen(['python', 'RunThis.py'])
 
-def load_config():
-    config_path = 'config.yaml'
-    if not os.path.exists(config_path):
-        create_default_config(config_path)
-    with open(config_path, 'r') as file:
-        return yaml.safe_load(file)
-    
-def create_default_config(config_path):
-    default_config = {
-        'tmdb_api_key': '',
-        'radarr_url': '',
-        'radarr_api_key': '',
-        'openai_api_key': '',
-        'discord_token': '',
-        'radarr_quality': '',
-        'selected_model': '',
-        'max_chars': 65540,
-        'discord_channel': '',
-        'radarr_root_folder': ''
-    }
-    with open(config_path, 'w') as file:
-        yaml.dump(default_config, file)
-    
-def initialize_radarr(config):
+def start_discord_bot_process():
+    subprocess.Popen(["python", "RunThis.py"])
+
+
+def initialize_radarr(ConfigManager.load_config):
     try:
-        if config['radarr_url'] and config['radarr_api_key']:
-            return RadarrAPI(config['radarr_url'], config['radarr_api_key'])
+        if config["radarr_url"] and config["radarr_api_key"]:
+            return RadarrAPI(config["radarr_url"], config["radarr_api_key"])
     except Exception as e:
         print(f"Error initializing Radarr API: {e}")
         return None
 
+
 config = load_config()
 # Set the root folder during initialization
 
-DISCORD_TOKEN = config['discord_token']
-TMDB_API_KEY = config['tmdb_api_key']
-RADARR_API_KEY = config['radarr_api_key']
-RADARR_URL = config['radarr_url']
-OPENAI_API_KEY = config['openai_api_key']
+DISCORD_TOKEN = config["discord_token"]
+TMDB_API_KEY = config["tmdb_api_key"]
+RADARR_API_KEY = config["radarr_api_key"]
+RADARR_URL = config["radarr_url"]
+OPENAI_API_KEY = config["openai_api_key"]
 RADARR_QUALITY = config["radarr_quality"]
-MAX_CHARS = config['max_chars']
-SELECTED_MODEL = config['selected_model']
-RADARR_ROOT_FOLDER = config['radarr_root_folder']
+MAX_CHARS = config["max_chars"]
+SELECTED_MODEL = config["selected_model"]
+RADARR_ROOT_FOLDER = config["radarr_root_folder"]
 
 
 # Function to load or create configuration
 def load_or_create_config():
-    config_path = 'config.yaml'
+    config_path = "config.yaml"
     default_config = {
-        'tmdb_api_key': '',
-        'radarr_url': '',
-        'radarr_api_key': '',
-        'openai_api_key': '',
-        'discord_token': '',
-        'radarr_quality': '',
-        'selected_model': '',
-        'max_chars': 65540,  # Default value for gpt-3.5-turbo-1106
-        'discord_channel': '',
-        'radarr_root_folder': ''
+        "tmdb_api_key": "",
+        "radarr_url": "",
+        "radarr_api_key": "",
+        "openai_api_key": "",
+        "discord_token": "",
+        "radarr_quality": "",
+        "selected_model": "",
+        "max_chars": 65540,  # Default value for gpt-3.5-turbo-1106
+        "discord_channel": "",
+        "radarr_root_folder": "",
     }
 
     if os.path.exists(config_path):
-        with open(config_path, 'r') as file:
+        with open(config_path, "r") as file:
             config = yaml.safe_load(file) or default_config
     else:
         config = default_config
-        with open(config_path, 'w') as file:
+        with open(config_path, "w") as file:
             yaml.dump(config, file)
 
     # Update MAX_CHARS based on the selected model
-    model = config.get('selected_model')
-    if model == 'gpt-4-1106-preview':
-        config['max_chars'] = 512000
+    model = config.get("selected_model")
+    if model == "gpt-4-1106-preview":
+        config["max_chars"] = 512000
 
     return config
-
-
 
 
 config = load_or_create_config()
 radarr = initialize_radarr(config)
 
 
-
-
 def start_bot(config):
     # Function to start the bot
-    os.system('python RunThis.py') 
+    os.system("python RunThis.py")
+
 
 def check_and_start_bot():
     config = load_or_create_config()
     # Check if the start_discord_bot_on_launch key is true
-    if config.get('start_discord_bot_on_launch', True):
-        required_keys = ['tmdb_api_key', 'radarr_url', 'radarr_api_key', 'openai_api_key', 'discord_token', 'radarr_quality', 'selected_model', 'max_chars', 'discord_channel']
-        if all(key in config and config[key] for key in required_keys): 
+    if config.get("start_discord_bot_on_launch", True):
+        required_keys = [
+            "tmdb_api_key",
+            "radarr_url",
+            "radarr_api_key",
+            "openai_api_key",
+            "discord_token",
+            "radarr_quality",
+            "selected_model",
+            "max_chars",
+            "discord_channel",
+        ]
+        if all(key in config and config[key] for key in required_keys):
             threading.Thread(target=start_bot, args=(config,)).start()
     else:
         print("Discord bot startup is disabled in the configuration.")
@@ -131,26 +120,29 @@ async def get_channels(token):
         channels_data.clear()
         for guild in client.guilds:
             for channel in guild.text_channels:
-                channels_data.append({'id': str(channel.id), 'name': channel.name})
+                channels_data.append({"id": str(channel.id), "name": channel.name})
 
         await client.close()
 
     await client.login(token)
     await client.start(token)
 
+
 def start_discord_client(token):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(get_channels(token))
 
-@app.route('/server_status')
+
+@app.route("/server_status")
 def server_status():
     # Simple status check. You can add more complex logic if needed.
-    return jsonify({'status': 'ready'})
+    return jsonify({"status": "ready"})
 
-@app.route('/channels', methods=['GET'])
+
+@app.route("/channels", methods=["GET"])
 def channels_endpoint():
-    token = request.args.get('token')
+    token = request.args.get("token")
     if token:
         thread = threading.Thread(target=start_discord_client, args=(token,))
         thread.start()
@@ -158,33 +150,36 @@ def channels_endpoint():
         print("Here is the channel data from the api call", channels_data)
     return jsonify(channels_data)
 
-@app.route('/load_config', methods=['GET'])
+
+@app.route("/load_config", methods=["GET"])
 def load_config_endpoint():
     # Call the load_config() function here to reload the configuration
     loaded_config = load_config()
-    
+
     # You can return a response or log a message if needed
-    print('Configuration reloaded on the server')
-    
-    return jsonify({'message': 'Configuration reloaded on the server'})
+    print("Configuration reloaded on the server")
+
+    return jsonify({"message": "Configuration reloaded on the server"})
 
 
-@app.route('/fetch_root_folders')
+@app.route("/fetch_root_folders")
 def fetch_root_folders():
     config = load_config()
-    radarr_url = config.get('radarr_url')
-    radarr_api_key = config.get('radarr_api_key')
+    radarr_url = config.get("radarr_url")
+    radarr_api_key = config.get("radarr_api_key")
 
     if radarr_url and radarr_api_key:
         try:
             url = f"{radarr_url}/api/v3/rootfolder"
-            headers = {'X-Api-Key': radarr_api_key}
+            headers = {"X-Api-Key": radarr_api_key}
             response = requests.get(url, headers=headers)
             if response.status_code == 200:
                 root_folders = response.json()
                 return jsonify(root_folders)
             else:
-                print(f"Error fetching Radarr root folders: HTTP {response.status_code}")
+                print(
+                    f"Error fetching Radarr root folders: HTTP {response.status_code}"
+                )
                 return jsonify([])
         except Exception as e:
             print(f"Error fetching Radarr root folders: {e}")
@@ -192,98 +187,104 @@ def fetch_root_folders():
 
     return jsonify([])
 
-        
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    
 
-    
-    if request.method == 'POST':
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
         # Update config from form data only if the input is not empty
-        for key in ['tmdb_api_key', 'radarr_url', 'radarr_api_key', 'openai_api_key', 'discord_token', 'radarr_quality', 'selected_model', 'discord_channel']:
+        for key in [
+            "tmdb_api_key",
+            "radarr_url",
+            "radarr_api_key",
+            "openai_api_key",
+            "discord_token",
+            "radarr_quality",
+            "selected_model",
+            "discord_channel",
+        ]:
             form_value = request.form.get(key)
             if form_value:  # Only update if the form input is not empty
                 config[key] = form_value
-        
-         # Update the discord_channel_id separately
-        discord_channel_id = request.form.get('discord_channel')
+
+        # Update the discord_channel_id separately
+        discord_channel_id = request.form.get("discord_channel")
         if discord_channel_id:
-            print("Received discord_channel_id:", discord_channel_id) # Logging statement
-            config['discord_channel_id'] = discord_channel_id
+            print(
+                "Received discord_channel_id:", discord_channel_id
+            )  # Logging statement
+            config["discord_channel_id"] = discord_channel_id
 
         # Handling the Radarr root folder
-        radarr_root_folder = request.form.get('radarr_root_folder')
+        radarr_root_folder = request.form.get("radarr_root_folder")
         if radarr_root_folder:
-            config['radarr_root_folder'] = radarr_root_folder
-        
+            config["radarr_root_folder"] = radarr_root_folder
+
         # Set MAX_CHARS based on the selected model
-        selected_model = config.get('selected_model')
-        if selected_model == 'gpt-3.5-turbo-1106':
-            config['max_chars'] = 65540
-        elif selected_model == 'gpt-4-1106-preview':
-            config['max_chars'] = 512000
+        selected_model = config.get("selected_model")
+        if selected_model == "gpt-3.5-turbo-1106":
+            config["max_chars"] = 65540
+        elif selected_model == "gpt-4-1106-preview":
+            config["max_chars"] = 512000
 
-        
-        
-
-        
-
-        
         # Restart the bot to apply new configuration
         threading.Thread(target=start_bot, args=(config,)).start()
 
-        start_discord_bot = request.form.get('start_discord_bot_on_launch') == 'on'
-        config['start_discord_bot_on_launch'] = start_discord_bot
+        start_discord_bot = request.form.get("start_discord_bot_on_launch") == "on"
+        config["start_discord_bot_on_launch"] = start_discord_bot
 
         # Write updated configuration to file
-        with open('config.yaml', 'w') as file:
+        with open("config.yaml", "w") as file:
             yaml.dump(config, file)
-          # Start the Discord bot in a separate process
-            
+        # Start the Discord bot in a separate process
 
-    
     radarr = None
-    if config.get('radarr_url') and config.get('radarr_api_key'):
+    if config.get("radarr_url") and config.get("radarr_api_key"):
         try:
-            radarr = RadarrAPI(config['radarr_url'], config['radarr_api_key'])
+            radarr = RadarrAPI(config["radarr_url"], config["radarr_api_key"])
         except Exception as e:
             print(f"Error initializing Radarr API: {e}")
 
     # Fetch root folders if RadarrAPI is initialized
     root_folders = []
 
-        
-    
     print("Root folders:", root_folders)
-    return render_template('index.html', config=config, root_folders=root_folders)
-
-if config.get('start_discord_bot_on_launch', True):
-        required_keys = ['tmdb_api_key', 'radarr_url', 'radarr_api_key', 'openai_api_key', 'discord_token', 'radarr_quality', 'selected_model', 'max_chars', 'discord_channel']
-        if all(key in config and config[key] for key in required_keys):
-            start_discord_bot_process()
+    return render_template("index.html", config=config, root_folders=root_folders)
 
 
-@app.route('/send_message', methods=['POST'])
+if config.get("start_discord_bot_on_launch", True):
+    required_keys = [
+        "tmdb_api_key",
+        "radarr_url",
+        "radarr_api_key",
+        "openai_api_key",
+        "discord_token",
+        "radarr_quality",
+        "selected_model",
+        "max_chars",
+        "discord_channel",
+    ]
+    if all(key in config and config[key] for key in required_keys):
+        start_discord_bot_process()
+
+
+@app.route("/send_message", methods=["POST"])
 def send_message():
-    message = request.json['message']
+    message = request.json["message"]
     print(f"Received message from UI: {message}")
 
-    if 'conversation_history' not in session:
-        session['conversation_history'] = []
+    if "conversation_history" not in session:
+        session["conversation_history"] = []
 
-    conversation_history = session['conversation_history']
+    conversation_history = session["conversation_history"]
 
     # Removed the line that adds the new message to conversation_history
     response = WebChat.get_openai_response(conversation_history, message)
 
-    session['conversation_history'] = conversation_history
-    return jsonify({'response': response})
+    session["conversation_history"] = conversation_history
+    return jsonify({"response": response})
 
 
-
-
-
-@app.route('/add_movie_to_radarr/<int:tmdb_id>', methods=['GET'])
+@app.route("/add_movie_to_radarr/<int:tmdb_id>", methods=["GET"])
 def add_movie_to_radarr(tmdb_id):
     config = load_config()
     radarr = initialize_radarr(config)
@@ -292,41 +293,35 @@ def add_movie_to_radarr(tmdb_id):
         return jsonify({"status": "error", "message": "Radarr is not configured."})
 
     tmdb = TMDb()
-    tmdb.api_key = config['tmdb_api_key']
+    tmdb.api_key = config["tmdb_api_key"]
     movie_api = Movie()
 
     try:
         movie_details = movie_api.details(tmdb_id)
         movie_title = movie_details.title
-        quality_profile_id = config["radarr_quality"]  # Ensure this returns the correct integer ID
+        quality_profile_id = config[
+            "radarr_quality"
+        ]  # Ensure this returns the correct integer ID
 
         # Add the movie to Radarr
         response = radarr.add_movie(
             tmdb_id=tmdb_id,
             quality_profile=quality_profile_id,
-            root_folder=config["radarr_root_folder"]
+            root_folder=config["radarr_root_folder"],
         )
 
         # Handle the response
         if response:  # Check if the response is successful
-            return jsonify({"status": "success", "message": f"*{movie_title}* added to Radarr"})
+            return jsonify(
+                {"status": "success", "message": f"*{movie_title}* added to Radarr"}
+            )
         else:
-            return jsonify({"status": "error", "message": "Failed to add movie to Radarr"})
+            return jsonify(
+                {"status": "error", "message": "Failed to add movie to Radarr"}
+            )
 
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)})
 
 
 # Rest of the code...
-
-
-
-
-
-
-
-
-
-
-
-    
---
+++ static/script.js
@@ -0,0 +1,298 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const configPanel = document.getElementById("config-panel");
+  const configToggle = document.getElementById("config-toggle");
+
+  configToggle.addEventListener("click", toggleConfigPanel);
+
+  document.addEventListener("click", function (event) {
+    var isClickInsideConfigPanel = configPanel.contains(event.target);
+    var isClickInsideConfigToggle = configToggle.contains(event.target);
+
+    if (
+      !isClickInsideConfigPanel &&
+      !isClickInsideConfigToggle &&
+      configPanel.style.left === "0px"
+    ) {
+      toggleConfigPanel(); // Close the panel
+    }
+  });
+});
+
+function handleRefresh(event) {
+  event.preventDefault(); // Prevent default form submission
+
+  // Gather form data
+  const formData = new FormData();
+  formData.append("radarr_url", document.getElementById("radarr_url").value);
+  formData.append(
+    "radarr_api_key",
+    document.getElementById("radarr_api_key").value
+  );
+  // ... add other form elements ...
+
+  // Send data using fetch
+  fetch("/", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => {
+      if (response.ok) {
+        checkServerStatusAndRefreshFolders();
+      } else {
+        console.error("Form submission failed");
+      }
+    })
+    .catch((error) => console.error("Error:", error));
+}
+
+function handleSubmit(event) {
+  event.preventDefault(); // Prevent default form submission
+
+  // Display a loading message
+  displayLoadingMessage();
+
+  // Gather form data
+  const formData = new FormData(document.querySelector("form"));
+
+  // Send data using fetch
+  fetch("/", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => {
+      if (response.ok) {
+        waitForServerReady(); // Wait for server to be ready then reload the page
+      } else {
+        console.error("Form submission failed");
+      }
+    })
+    .catch((error) => console.error("Error:", error));
+}
+
+function displayLoadingMessage() {
+  // Add a loading message to the page
+  let loadingDiv = document.createElement("div");
+  loadingDiv.id = "loadingMessage";
+  loadingDiv.style.position = "fixed";
+  loadingDiv.style.top = "50%";
+  loadingDiv.style.left = "50%";
+  loadingDiv.style.transform = "translate(-50%, -50%)";
+  loadingDiv.style.fontSize = "1.5rem";
+  loadingDiv.innerText = "Configuration is updating. Please wait...";
+  document.body.appendChild(loadingDiv);
+}
+
+function waitForServerReady() {
+  fetch("/server_status")
+    .then((response) => {
+      if (response.ok) {
+        window.location.reload(); // Reload the page
+      } else {
+        setTimeout(waitForServerReady, 1000); // Retry after 1 second
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      setTimeout(waitForServerReady, 1000);
+    });
+}
+
+function markdownToHTML(text) {
+  // Convert italics
+  let htmlText = text.replace(/\*([^\*]+)\*/g, "<em>$1</em>");
+
+  // Convert bullet points with indentation
+  htmlText = htmlText.replace(
+    /^- (.+)$/gm,
+    "<li style='margin-left: 20px;'>$1</li>"
+  );
+  htmlText = "<ul>" + htmlText + "</ul>"; // Wrap with <ul> tag
+
+  return htmlText;
+}
+
+function checkServerStatusAndRefreshFolders() {
+  fetch("/server_status")
+    .then((response) => {
+      if (response.ok) {
+        // Server is ready, now refresh Radarr root folders
+        fetchRadarrRootFolders();
+      } else {
+        setTimeout(checkServerStatusAndRefreshFolders, 1000); // Retry after 1 second
+      }
+    })
+    .catch((error) => {
+      console.error("Error checking server status:", error);
+      setTimeout(checkServerStatusAndRefreshFolders, 1000); // Retry after 1 second
+    });
+}
+
+function waitForServerReady() {
+  fetch("/server_status")
+    .then((response) => {
+      if (response.ok) {
+        // Server is ready, reset UI
+        resetUI();
+      } else {
+        // Retry after a delay
+        setTimeout(waitForServerReady, 3000); // Retry after 3 seconds
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      setTimeout(waitForServerReady, 3000);
+    });
+}
+
+function resetUI() {
+  // Hide loading indicator
+  document.getElementById("loadingIndicator").style.display = "none";
+  // Enable submit button
+  document.getElementById("submitBtn").disabled = false;
+}
+
+function fetchRadarrRootFolders() {
+  let radarrUrl = document.getElementById("radarr_url").value;
+  let radarrApiKey = document.getElementById("radarr_api_key").value;
+
+  if (radarrUrl && radarrApiKey) {
+    fetch(
+      `/fetch_root_folders?radarr_url=${encodeURIComponent(
+        radarrUrl
+      )}&radarr_api_key=${encodeURIComponent(radarrApiKey)}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        let rootFolderSelect = document.getElementById("radarrRootFolder");
+        rootFolderSelect.innerHTML = ""; // Clear existing options
+
+        data.forEach((folder) => {
+          let option = document.createElement("option");
+          option.value = folder.path;
+          option.textContent = folder.path;
+          rootFolderSelect.appendChild(option);
+        });
+      })
+      .catch((error) => console.error("Error:", error));
+  }
+}
+
+async function loadChannels() {
+  let token = document.getElementById("discord_token").value;
+  if (token) {
+    let response = await fetch("/channels?token=" + encodeURIComponent(token));
+    if (response.ok) {
+      let channels = await response.json();
+      console.log("Here is the json object I think:", channels);
+      let channelSelect = document.getElementById("discord_channel");
+      channelSelect.innerHTML = "";
+
+      channels.forEach((channel) => {
+        let option = document.createElement("option");
+        option.value = channel.id;
+        option.textContent = channel.name;
+        console.log("Channel name: ", channel.name);
+        console.log("Channel id: ", channel.id);
+        channelSelect.appendChild(option);
+      });
+      channelSelect.disabled = false;
+    }
+  }
+}
+
+function handleKeyPress(event) {
+  if (event.keyCode === 13) {
+    // 13 is the Enter key
+    event.preventDefault(); // Prevent default to avoid form submit
+    sendMessage();
+  }
+}
+
+function toggleConfigPanel() {
+  var configPanel = document.getElementById("config-panel");
+  if (configPanel.style.left === "0px") {
+    configPanel.style.left = "-250px"; // Hide the panel
+  } else {
+    configPanel.style.left = "0px"; // Show the panel
+  }
+}
+
+function sendMessage() {
+  let messageInput = document.getElementById("message-input");
+  let message = messageInput.value;
+  updateChat("user", message);
+
+  // Clear the input box right after sending the message
+  messageInput.value = "";
+
+  fetch("/send_message", {
+    method: "POST",
+    body: JSON.stringify({ message: message }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      updateChat("bot", data.response);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function refreshDiscordChannels() {
+  let token =
+    document.getElementById("discord_token").value ||
+    '{{ config["discord_token"] }}';
+  if (token) {
+    // Existing code to load channels
+    loadChannels();
+  } else {
+    alert("Please enter a Discord token.");
+  }
+}
+
+function checkAndFetchRootFolders() {
+  let radarrUrl = document.getElementById("radarr_url").value;
+  let radarrApiKey = document.getElementById("radarr_api_key").value;
+
+  if (radarrUrl && radarrApiKey) {
+    fetchRadarrRootFolders();
+  }
+}
+
+function updateChat(sender, text) {
+  let chatBox = document.getElementById("chat-box");
+  let messageDiv = document.createElement("div");
+  messageDiv.classList.add("message");
+
+  let senderSpan = document.createElement("span");
+  senderSpan.classList.add(sender === "user" ? "sender-user" : "sender-bot");
+  senderSpan.textContent = sender === "user" ? "You: " : "Bot: ";
+
+  let textDiv = document.createElement("div");
+  textDiv.classList.add("text-content");
+
+  // Convert markdown to HTML
+  let htmlContent = markdownToHTML(text);
+
+  // Set the innerHTML of textDiv to the processed HTML content
+  textDiv.innerHTML = htmlContent;
+
+  messageDiv.appendChild(senderSpan);
+  messageDiv.appendChild(textDiv);
+
+  chatBox.appendChild(messageDiv);
+  chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+function addMovieToRadarr(tmdbId) {
+  fetch(`/add_movie_to_radarr/${tmdbId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      // Instead of alert, display the message in chat
+      updateChat("bot", data.message);
+    })
+    .catch((error) => console.error("Error:", error));
+}
